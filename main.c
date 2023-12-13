0c57e647b3ff806813afd4d3a94c342cfdc3cf52
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2023-12-13 23:49:18 +0200

    fix

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	read_monty(argv[1]);
